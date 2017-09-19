80656d133107b9f55528bd88a1b977ba040e7bf8
1 <1>
2017-09-19 12:05:09 +0800

    2017.9.19_2

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,14 +1,30 @@
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
 public class MainFrame extends Frame {
+    private Button btnExit = new Button("Exit");
     public MainFrame(){
         initComp();
     }
     private void initComp(){
-        this.setLocation(100,120);
-        this.setSize(400,300);
+//        this.setLocation(100,120);
+//        this.setSize(400,300);
+        this.setBounds(100,120,400,300);
+        this.setLayout(null);
+        btnExit.setBounds(150,110,80,30);
+        btnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+
+            }
+        });
+
+
+        this.add(btnExit);
         this.addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
