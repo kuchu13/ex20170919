b2a8737c09561945a94d415b9c1ec3e283859612
1 <1>
2017-09-19 12:37:30 +0800

    2017.9.19_3

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -3,7 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
-//ait+Enter
+//alt+Enter
 
 public class MainFrame extends Frame {
     private Button btnExit = new Button("Exit");
